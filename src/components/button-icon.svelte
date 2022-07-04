14240407a848015e2ea820694f85b3095821a1bc
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2022-07-04 15:39:50 -0500

    :construction: wip: create room/chat

--- src/components/button-icon.svelte
+++ src/components/button-icon.svelte
@@ -1,9 +1,12 @@
 <script>
-	/** @property {function} click*/
+	/** @property {function}*/
 	export let click = () => console.log('Hello, Peter!');
 
-	/** @property {boolean} click*/
+	/** @type {boolean}*/
 	export let transparent = false;
+
+	/** @type {string}*/
+	export let css = '';
 
 	const bgColor = 'bg-blue-600 hover:bg-blue-700 active:bg-blue-800';
 	const bgTransparent = 'bg-transparent hover:text-neutral-400 active:text-neutral-600';
@@ -11,7 +14,7 @@
 
 <button
 	class="text-neutral-200 w-10 h-10 rounded-full transition-colors flex items-center justify-center
-  {transparent ? bgTransparent : bgColor}"
+  {transparent ? bgTransparent : bgColor} {css}"
 	on:click={click}
 >
 	<slot />
--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -5,9 +5,9 @@
 	import { userLogged } from '../stores/session';
 	import { getAccessToken } from '../services/user';
 
-	import Button from '../components/button.svelte';
 	import ButtonIcon from '../components/button-icon.svelte';
 	import IconLogout from '../components/icons/icon-logout.svelte';
+	import IconPlus from '../components/icons/icon-plus.svelte';
 
 	import '../app.css';
 	import { onMount } from 'svelte';
@@ -25,7 +25,7 @@
 </script>
 
 {#if $userLogged}
-	<div class="flex flex-col gap-3 max-w-md mx-auto pt-4">
+	<div class="flex flex-col gap-3 max-w-md mx-auto p-4 h-full relative">
 		<header class="flex flex-row gap-3 items-center justify-between">
 			<div class="flex flex-row gap-3 items-center">
 				<img class="rounded-full w-8" src={$userLogged?.avatar} alt="" />
@@ -37,5 +37,8 @@
 				<IconLogout width={20} height={20} />
 			</ButtonIcon>
 		</header>
+		<ButtonIcon css="absolute bottom-6 right-6">
+			<IconPlus />
+		</ButtonIcon>
 	</div>
 {/if}
