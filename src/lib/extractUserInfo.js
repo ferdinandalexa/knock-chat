1f493a9d244625f061c3738bded4922920dd8fda
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2022-07-05 11:04:13 -0500

    :recycle: refactor: extract user info from google and github session

--- src/lib/extractUserInfo.js
+++ src/lib/extractUserInfo.js
@@ -7,28 +7,15 @@ export default function extractUserInfo(userData, accessToken) {
 		return null;
 	}
 
-	const { provider, identity_data } = userData.identities[0];
+	const { avatar_url, user_name } = userData.user_metadata;
 
 	/** @type {import('../types/User').User | null} */
-	let user = null;
-
-	if (provider === 'google') {
-		user = {
-			name: identity_data?.name,
-			avatar: identity_data?.avatar_url,
-			token: accessToken,
-			chatToken: ''
-		};
-	}
-
-	if (provider === 'github') {
-		user = {
-			name: identity_data?.user_name,
-			avatar: identity_data?.avatar_url,
-			token: accessToken,
-			chatToken: ''
-		};
-	}
+	const user = {
+		name: user_name,
+		avatar: avatar_url,
+		token: accessToken,
+		chatToken: ''
+	};
 
 	return user;
 }
