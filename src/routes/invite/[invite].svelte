37caf93442a9a64b24b4260a51db735d8e4f3a28
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2022-07-06 20:08:52 -0500

    :ambulance: fix: add vite config
    clear

--- src/routes/invite/[invite].svelte
+++ src/routes/invite/[invite].svelte
@@ -1,10 +1,4 @@
 <script>
-	import { goto } from '$app/navigation';
-
-	import { activeConversation } from '$stores/chat';
-	import { userLogged } from '$stores/session';
-	import { deleteChat } from '$services/chat';
-
 	import Button from '$components/button.svelte';
 </script>
 
