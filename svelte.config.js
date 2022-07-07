03fd3c1e2f2d9387657deb0857da596c4ffb1f06
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2022-07-06 20:49:59 -0500

    :ambulance: fix: move vite resolves into vite config

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,10 @@
-import path from 'path';
-
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		vite: {
-			resolve: {
-				alias: {
-					$components: path.resolve('src/components'),
-					$icons: path.resolve('src/components/icons'),
-					$containers: path.resolve('src/containers'),
-					$routes: path.resolve('src/routes'),
-					$stores: path.resolve('src/stores'),
-					$services: path.resolve('src/services')
-				}
-			}
-		}
+		adapter: adapter()
 	},
 	preprocess: [
 		preprocess({
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
+import path from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$components: path.resolve('src/components'),
+			$icons: path.resolve('src/components/icons'),
+			$containers: path.resolve('src/containers'),
+			$routes: path.resolve('src/routes'),
+			$stores: path.resolve('src/stores'),
+			$services: path.resolve('src/services')
+		}
+	}
 };
 
 export default config;
