3cec48ffb49f8f7a7abfc7a38cee0314aca48d62
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2022-07-16 16:37:30 -0500

    :sparkles: feat: send message when user keypress enter

--- src/components/conversation-input.svelte
+++ src/components/conversation-input.svelte
@@ -1,6 +1,6 @@
 <script>
-	import autosize from 'autosize';
 	import { onMount } from 'svelte';
+	import autosize from 'autosize';
 
 	import { activeConversation } from '$stores/chat';
 
@@ -12,10 +12,25 @@
 	/**@type {HTMLTextAreaElement}*/
 	let textarea;
 
-	function handleSubmit() {
-		if ($activeConversation != null) {
+	function sendMessage() {
+		const validatedString = message.match(/.+/g);
+		if ($activeConversation != null && validatedString) {
 			$activeConversation.sendMessage(message);
 			message = '';
+			textarea.value = message;
+		}
+	}
+
+	function handleSubmit() {
+		sendMessage();
+	}
+
+	/**@param {KeyboardEvent} event*/
+	function handleEnter(event) {
+		if (event.key === 'Enter' && !event.shiftKey) {
+			sendMessage();
+			/**@ts-ignore*/
+			autosize.update(textarea);
 		}
 	}
 
@@ -24,13 +39,18 @@
 	});
 </script>
 
-<form on:submit|preventDefault={handleSubmit} class="p-4 w-full bg-neutral-800">
+<form
+	on:submit|preventDefault={handleSubmit}
+	on:keyup={handleEnter}
+	class="p-4 w-full bg-neutral-800"
+>
 	<div class="flex flex-row gap-2 items-end">
 		<textarea
 			bind:value={message}
 			bind:this={textarea}
 			type="text"
 			placeholder="knock knock"
+			rows="0"
 			class="bg-neutral-600 placeholder-neutral-400 border-2 border-neutral-500 transition-colors h-4 resize-none max-h-[10ch] rounded-md w-full py-2 px-4 text-white leading-tight focus:outline-none focus:border-slate-400 flex-1"
 			required
 		/>
