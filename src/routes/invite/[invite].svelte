9e0830ac4ec83fd8ccebede271ba33bb20060c8c
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2022-07-19 17:23:15 -0500

    :sparkles: feat: user can join conversation by invite

--- src/routes/invite/[invite].svelte
+++ src/routes/invite/[invite].svelte
@@ -1,5 +1,55 @@
 <script>
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+
+	import { addParticipant } from '$services/chat';
+	import { userLogged } from '$stores/session';
+
+	import { onMount } from 'svelte';
+
+	import { supabase } from '$lib/supabase';
+	import { getUserInfo } from '$lib/getUserInfo';
+	import { setUserInfo } from '$lib/setUserInfo';
+
 	import Button from '$components/button.svelte';
+
+	import '../../app.css';
+
+	/** @typedef {import('@supabase/supabase-js').Session | null} Session*/
+
+	/** @type {boolean}*/
+	let isLoading = true;
+
+	/** @type {Session}*/
+	const session = supabase.auth.session();
+
+	supabase.auth.onAuthStateChange(async (event, session) => {
+		if (event === 'SIGNED_IN' && session != null) await getUserInfo(session);
+
+		if (event === 'SIGNED_OUT') setUserInfo(null, false);
+
+		isLoading = false;
+	});
+
+	onMount(async () => {
+		if (session != null) await getUserInfo(session);
+
+		isLoading = false;
+	});
+
+	const roomSID = $page.params.invite;
+
+	async function handleJoin() {
+		if ($userLogged != null) {
+			const accessToken = $userLogged?.chatToken;
+			const username = $userLogged?.name;
+			await addParticipant(accessToken, roomSID, username);
+			goto('/');
+		}
+	}
 </script>
 
-<Button>Unirse a la conversación</Button>
+<div class="h-full w-full grid place-items-center">
+	<p class="text-white">{roomSID}</p>
+	<Button on:click={handleJoin}>Unirse a la conversación</Button>
+</div>
